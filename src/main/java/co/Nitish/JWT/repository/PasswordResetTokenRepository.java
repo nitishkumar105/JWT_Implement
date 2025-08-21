77dda9eed6cf7bfac0bd1e378a2ccafa9894e727
Nitish Kumar <nitishkumaryadav105@gmail.com>
2025-08-21 17:09:46 +0530

    use password and username in env varibale and also add in gitignore

--- src/main/java/co/Nitish/JWT/repository/PasswordResetTokenRepository.java
+++ src/main/java/co/Nitish/JWT/repository/PasswordResetTokenRepository.java
@@ -3,8 +3,11 @@ package co.Nitish.JWT.repository;
 import co.Nitish.JWT.entity.PasswordResetToken;
 import co.Nitish.JWT.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
     Optional<PasswordResetToken> findByToken(String token);
     Optional<PasswordResetToken> findByUser(User user);
